4dcffa0a9e5c271e62070ad6fe2a18538ffe286e
aidanmc95 <aidanmc95@gmail.com>
2020-10-22 15:51:49 -0700

    Grab email by subject

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import os
 import email
 from decouple import config
 
-email = config('email')
+email_user = config('email')
 email_password = config('emailpassword')
+subject = 'Reporting Flow'
 
 print(email, email_password)
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login(email, email_password)
+mail.login(email_user, email_password)
 mail.select('Inbox')
 
-# type, data = mail.search(None, 'ALL')
-# mail_ids = data[0]
-# id_list = mail_ids.split()
+type, data = mail.search(None,'(SUBJECT "%s")' % subject)
+mail_ids = data[0]
+id_list = mail_ids.split()
+
+typ, data = mail.fetch(id_list[0], '(RFC822)' )
+raw_email = data[0][1]
+
+raw_email_string = raw_email.decode('utf-8')
+email_message = email.message_from_string(raw_email_string)
+
+print(raw_email_string)
+
+if __name__ == '__main__':
+    print('hi')
